55d5154db92da40fea5f6bc2eaf69fe0d0c21b4d
zkrakus <zkrakus@gmail.com>
2023-04-12 19:16:44 -0400

    Clean up structure. Added concept of entity. Dawn of the first day.

---
+++ src/CloudCake/Domain/Entities/Entity.cs
@@ -0,0 +1,98 @@
+﻿using System.Reflection;
+
+namespace CloudCake.Domain.Entities
+{
+    /// <summary>
+    /// A shortcut of <see cref="Entity{TPrimaryKey}"/> for most used primary key type (<see cref="int"/>).
+    /// </summary>
+    [Serializable]
+    public abstract class Entity : Entity<int>, IEntity
+    {
+        protected Entity(int id) : base(id)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Basic implementation of IEntity interface.
+    /// An entity can inherit this class of directly implement to IEntity interface.
+    /// </summary>
+    /// <typeparam name="TPrimaryKey">Type of the primary key of the entity</typeparam>
+    [Serializable]
+    public abstract class Entity<TPrimaryKey> : IEntity<TPrimaryKey>
+    {
+        /// <summary>
+        /// Unique identifier for this entity.
+        /// </summary>
+        public virtual TPrimaryKey Id { get; set; }
+
+        protected Entity(TPrimaryKey id)
+        {
+            Id = id;
+        }
+
+        /// <summary>
+        /// Checks if this entity is transient (it does has not have an Id).
+        /// </summary>
+        /// <returns>True, if this entity is transient</returns>
+        public virtual bool IsTransient()
+        {
+            if (EqualityComparer<TPrimaryKey>.Default.Equals(Id, default))
+            {
+                return true;
+            }
+
+            //Workaround for EF Core since it sets int/long to min value when attaching to dbcontext
+            if (typeof(TPrimaryKey) == typeof(int))
+            {
+                return Convert.ToInt32(Id) <= 0;
+            }
+
+            if (typeof(TPrimaryKey) == typeof(long))
+            {
+                return Convert.ToInt64(Id) <= 0;
+            }
+
+            return false;
+        }
+
+        public virtual bool EntityEquals(object obj)
+        {
+            if (obj == null || obj is not Entity<TPrimaryKey>)
+            {
+                return false;
+            }
+
+            //Same instances must be considered as equal
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            //Transient objects are not considered as equal
+            var other = (Entity<TPrimaryKey>)obj;
+            if (IsTransient() && other.IsTransient())
+            {
+                return false;
+            }
+
+            //Must have a IS-A relation of types or must be same type
+            var typeOfThis = GetType();
+            var typeOfOther = other.GetType();
+            if (!typeOfThis.GetTypeInfo().IsAssignableFrom(typeOfOther) && !typeOfOther.GetTypeInfo().IsAssignableFrom(typeOfThis))
+            {
+                return false;
+            }
+            
+            if(Id is not null)
+                return Id.Equals(other.Id);
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"[{GetType().Name} {Id}]";
+        }
+    }
+}
